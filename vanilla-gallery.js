a069b7744f245eaed4bf9510bc5bc5f1943fc3ba
hanif.ifarmer <hanif@ifarmer.asia>
2023-08-23 03:18:42 +0600

    beta 01

--- vanilla-gallery.js
+++ vanilla-gallery.js
@@ -1,4 +1,33 @@
 // DOM elements
+const vanillaGallery = document.querySelector('.vanila-gallery');
+const modalHTML = `
+<div class="gallery-modal">
+<div class="modal-btns">
+  <button id="prev-btn">&lt;</button>
+  <button id="next-btn">&gt;</button>
+</div>
+<div class="modal-section">
+  <div class="img-part">
+    <img>
+  </div>
+  <div class="title-part">
+    <div class="title-section">
+      <div class="modal-text">
+        <p></p>
+      </div>
+      <div class="title-btns">
+        <a class="modal-download-link" href="" download="">
+          <button class="modal-download-btn"><span><img src="ico/download.png"></span> <span>Download</span></button>
+        </a>
+        <button class="cross-btn" onclick="hideModal()"><img src="ico/cross.png"></button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`;
+vanillaGallery.innerHTML += modalHTML;
+
 const allGalleryCards = document.querySelectorAll('.vanila-gallery .gallery-card');
 const galleryModal = document.querySelector('.gallery-modal');
 const downloadBtn = document.querySelectorAll('.download-btn');
@@ -27,24 +56,19 @@ downloadBtn.forEach((btn) => {
 
 // onclick img popup
 allGalleryCards.forEach((galleryCard, i) => {
-  galleryCard.addEventListener('click', () => {
+  galleryCard.addEventListener('click', (event) => {
     galleryModal.querySelector('img').src = galleryCardInfo(galleryCard).imgSrc;
+    galleryModal.querySelector('.title-btns .modal-download-link').href = galleryCardInfo(galleryCard).imgSrc;
     if(galleryModal.querySelector('.modal-section .title-part p').innerHTML != null) {
       galleryModal.querySelector('.modal-section .title-part p').innerHTML = galleryCardInfo(galleryCard).title;
     }
-    currCardIndex = i;
-    galleryModal.style.visibility = 'visible';
+    if(!event.target.closest('.download-btn')){
+      currCardIndex = i;
+      galleryModal.style.visibility = 'visible';
+    }
   });
 });
 
-// hide modal
-// document.addEventListener('click', function(event) {
-//   if (event.target.closest('.gallery-modal .modal-btns')) {
-//     galleryModal.style.visibility = 'hidden';
-//     galleryModal.querySelector('img').src = '';
-//   }
-//   console.log(event.target);
-// });
 
 // image swipe
 const prevBtn = document.getElementById('prev-btn');
@@ -57,9 +81,9 @@ nextBtn.addEventListener('click', () => {
   else {
     currCardIndex += 1;
   }
-  console.log(currCardIndex);
 
   galleryModal.querySelector('img').src = galleryCardsInfo()[currCardIndex].imgSrc;
+  galleryModal.querySelector('.title-btns .modal-download-link').href = galleryCardsInfo()[currCardIndex].imgSrc;
   if(galleryModal.querySelector('p').innerHTML != null) {
     galleryModal.querySelector('p').innerHTML = galleryCardsInfo()[currCardIndex].title;
   }
@@ -72,16 +96,22 @@ prevBtn.addEventListener('click', () => {
   else {
     currCardIndex -= 1;
   }
-  console.log(currCardIndex);
   galleryModal.querySelector('img').src = galleryCardsInfo()[currCardIndex].imgSrc;
+  galleryModal.querySelector('.title-btns .modal-download-link').href = galleryCardsInfo()[currCardIndex].imgSrc;
   if(galleryModal.querySelector('p').innerHTML != null) {
     galleryModal.querySelector('p').innerHTML = galleryCardsInfo()[currCardIndex].title;
   }
 });
-// modal download
-
 
 // hide modal function
 function hideModal(){
   galleryModal.style.visibility = 'hidden';
 }
+
+// hide modal
+document.addEventListener('click', function(event) {
+  if (event.target.closest('.gallery-modal .modal-btns') && !event.target.closest('#prev-btn') && !event.target.closest('#next-btn')) {
+    galleryModal.style.visibility = 'hidden';
+    galleryModal.querySelector('img').src = '';
+  }
+});
